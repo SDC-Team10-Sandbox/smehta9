051c579f84bb05e312c29b5c24c712fa25fefe53
Myles Gregor <30939472+mylesgregor@users.noreply.github.com>
2024-03-18 13:57:35 -0400

    Auto-generated from CI/CD pipeline generator

---
+++ test/smehta9.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Smehta9 from '../lib/smehta9-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/smehta9-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Smehta9.Smehta9Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
