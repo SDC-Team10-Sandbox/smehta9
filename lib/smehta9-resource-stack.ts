ab72096c66aa24951812fcaeee3655fc02c32ddc
migregor <migregor@ncsu.edu>
2024-03-20 15:06:34 -0400

    testing sqs

--- lib/smehta9-resource-stack.ts
+++ lib/smehta9-resource-stack.ts
@@ -5,11 +5,16 @@ import * as sfn from 'aws-cdk-lib/aws-stepfunctions';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as tasks from 'aws-cdk-lib/aws-stepfunctions-tasks';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambdaEventSources from 'aws-cdk-lib/aws-lambda-event-sources';
+
 import path = require('path');
 
 export class MyLambdaStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
+
+        const queue = new sqs.Queue(this, 'SampleQueue');
         
         const shellLambda = new Function(this, 'LambdaFunction', {
             code: lambda.Code.fromAsset(path.resolve(__dirname, "lambdas")),
@@ -18,6 +23,10 @@ export class MyLambdaStack extends cdk.Stack {
             timeout: cdk.Duration.seconds(3)
         });
 
+        const eventSource = new lambdaEventSources.SqsEventSource(queue);
+
+        shellLambda.addEventSource(eventSource);
+
         //Shell Step Function Definition
         const stateMachine = new sfn.StateMachine(this, 'MyStateMachine', {
             definition: new tasks.LambdaInvoke(this, "MyLambdaTask", {
@@ -25,22 +34,27 @@ export class MyLambdaStack extends cdk.Stack {
             }).next(new sfn.Succeed(this, "GreetedWorld"))
           });
 
-    // Define an API Gateway
+
     
-    const api = new apigateway.RestApi(this, 'MyLambdaApi', {
+    
+        const api = new apigateway.RestApi(this, 'MyLambdaApi', {
         restApiName: 'My Lambda API',
         description: 'This is an example API for Lambda integration',
-      });
+        });
   
       // Define a resource (root of the API)
-      const resource = api.root.addResource('myresource');
-  
-      // Define a method (e.g., GET) on the resource
-      const method = resource.addMethod('GET', new apigateway.LambdaIntegration(shellLambda));
-  
-      // Output the API endpoint URL
-      new cdk.CfnOutput(this, 'ApiEndpoint', {
-        value: api.url,
-      });
+        const resource = api.root.addResource('myresource');
+    
+        // Define a method (e.g., GET) on the resource
+        const method = resource.addMethod('GET', new apigateway.LambdaIntegration(shellLambda));
+    
+        // Output the API endpoint URL
+        new cdk.CfnOutput(this, 'ApiEndpoint', {
+            value: api.url,
+        });
+
+        
     }
+
 }
+
